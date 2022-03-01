b6a9352a610ddf20a3dc02f58b6c04ff61140e8f
Jefferson Schmitt <jeffersonschmitt@live.com>
2022-03-01 20:39:28 -0300

    loja-tutorial-youtube

---
+++ store/app/components/product.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ProductComponent extends Component {
+  // @tracked number = 0;
+  // @action
+  // addNumber() {
+  //     this.number = this.number + 1;
+  // }
+  productImage = this.args.product.colors[0].image;
+}
